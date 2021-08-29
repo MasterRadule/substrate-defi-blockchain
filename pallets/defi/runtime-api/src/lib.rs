aeba3ad02d006da3091b43fa98c36983eb7a8534
Danijel Radulović <master.daca09@gmail.com>
2021-08-29 14:37:43 +0200

    Delete unnecessary BorrowingInfo

--- pallets/defi/runtime-api/src/lib.rs
+++ pallets/defi/runtime-api/src/lib.rs
@@ -28,34 +28,6 @@ pub struct BalanceInfo<Balance> {
     pub balance: Balance,
 }
 
-#[derive(Eq, PartialEq, Encode, Decode, Default)]
-#[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
-#[cfg_attr(feature = "std", serde(rename_all = "camelCase"))]
-pub struct BorrowingInfo<Balance> {
-	#[cfg_attr(
-		feature = "std",
-		serde(
-			bound(
-				serialize = "Balance: std::fmt::Display",
-				deserialize = "Balance: std::str::FromStr"
-			),
-			with = "string_serialization"
-		)
-	)]
-	pub borrowing_balance: Balance,
-	#[cfg_attr(
-		feature = "std",
-		serde(
-			bound(
-				serialize = "Balance: std::fmt::Display",
-				deserialize = "Balance: std::str::FromStr"
-			),
-			with = "string_serialization"
-		)
-	)]
-	pub allowed_borrowing_amount: Balance,
-}
-
 sp_api::decl_runtime_apis! {
 	pub trait DefiModuleAPI<AccountId, Balance> where
 		AccountId: Codec,
